0e0bfcf456cb62db089bd999238f7c157f6deb81
高倩 <942246726@qq.com>
2026-05-24 09:32:22 +0100

    linkedlist

---
+++ src/linkedlist/copyRandomList.java
@@ -0,0 +1,22 @@
+package src.linkedlist;
+
+
+import java.util.HashMap;
+
+public class copyRandomList {
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
