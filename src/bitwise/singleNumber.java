347f375ce5489d85107a2130f3afc3e05cdaf50c
高倩 <942246726@qq.com>
2026-06-17 09:28:46 +0100

    bitwise

---
+++ src/bitwise/singleNumber.java
@@ -0,0 +1,11 @@
+package src.bitwise;
+
+public class singleNumber {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for (int i = 0; i < nums.length; i++) {
+            ans^=nums[i];
+        }
+        return ans;
+    }
+}
