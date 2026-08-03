9c8b65b1e21e179b8dccb95690c1b021647bef88
高倩 <942246726@qq.com>
2026-08-03 19:45:46 +0100

    linkedLIST

---
+++ src/linkedlist/mergeTwoLists.java
@@ -0,0 +1,22 @@
+package src.linkedlist;
+
+import src.ListNode;
+
+public class mergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode();
+        ListNode cur=dummy;
+        while(list1!=null&&list2!=null){
+            if(list1.val<list2.val){
+                cur.next=list1;
+                list1=list1.next;
+            }else {
+                cur.next=list2;
+                list2=list2.next;
+            }
+            cur=cur.next;//不要少了这一步
+        }
+       cur.next= list1!=null?list1:list2;//拼接剩余链表
+        return dummy.next;
+    }
+}
