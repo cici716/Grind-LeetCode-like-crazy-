58d02c054340ebe6f1982e482a9bec9cffd243a2
高倩 <942246726@qq.com>
2026-08-27 19:51:11 +0100

    dp

---
+++ src/dp/longestValidParentheses.java
@@ -0,0 +1,25 @@
+package src.dp;
+
+public class longestValidParentheses {
+    public int longestValidParentheses(String s) {
+        if (s==null||s.length()<=2) return 0;
+        int n=s.length();
+        int[]dp=new int[n];
+        int maxLen=0;
+
+        for (int i=1;i<n;i++){
+            if (s.charAt(i)==')'){
+                if (s.charAt(i-1)=='('){
+                    dp[i]=(i>=2?dp[i-2]:0)+2;
+                }else {
+                    int pre=i-dp[i-1]-1;
+                    if (pre>=0&&s.charAt(pre)=='('){
+                        dp[i]=dp[i-1]+2+(pre>=1?dp[pre-1]:0);
+                    }
+                }
+                maxLen=Math.max(maxLen,dp[i]);
+            }
+        }
+        return maxLen;
+    }
+}
